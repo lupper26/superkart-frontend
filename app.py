57684749ae7167bb7fbed97120c505d1e81f4eca
lupper26 <lupper317@gmail.com>
2026-08-16 14:48:53 -0400

    Correct date and time formatting in app.py
    Fix formatting of the current date and time display.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if "backend_warmed" not in st.session_state:
             st.session_state["backend_warmed"] = False
 
 st.title("SuperKart Retail Sales Forecasting Dashboard")
-st.write(f"Current date and time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+st.write(f"Current date& time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 # Input fields for product and store data
 Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
 Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
